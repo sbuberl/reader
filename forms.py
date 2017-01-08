0d6360bdfbc4489d7cb56bde54550268ffdbe73a
sbuberl <sbuberl@gmail.com>
2017-01-08 13:11:58 -0500

    Add Information edit to library items
    Fix release date to be optional
    Adds cancel button to edit meta screen

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, IntegerField
 from wtforms.fields.html5 import DateField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 
 
 class UploadForm(FlaskForm):
@@ -18,12 +18,12 @@ class ComicMetadataForm(FlaskForm):
     issue = IntegerField('Issue', validators=[DataRequired()])
     author = StringField('Author')
     publisher = StringField('Publisher')
-    release_date = DateField('Release Date')
+    release_date = DateField('Release Date', validators=[Optional()])
 
 
 class DocumentMetadataForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     author = StringField('Author')
     publisher = StringField('Publisher')
-    release_date = DateField('Release Date')
+    release_date = DateField('Release Date', validators=[Optional()])
 
