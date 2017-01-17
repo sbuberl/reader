b84f68775c40d7a9f8731adecf2ced91971f38de
sbuberl <sbuberl@gmail.com>
2017-01-16 23:12:12 -0500

    Added react-router

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
+//import Router from './router';
+import ComicReader from './comic_reader';
 import Library from './library';
+import UploadForm from './upload';
 
 ReactDOM.render(
-    <Library />,
+    <Router history={hashHistory}>
+        <Route path="/" component={Library}/>
+        <Route path="/upload" component={UploadForm}/>
+        <Route path="/read/comic/:comic" component={ComicReader}/>
+    </Router>,
     document.getElementById('example')
 );
--- js/library.js
+++ js/library.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import 'whatwg-fetch';
 
 export default class Library extends React.Component {
@@ -30,13 +31,14 @@ export default class Library extends React.Component {
                     <h1 className="page-header">Library</h1>
                 </div>
             </div>
+            <div className="row pull-right"><Link to="/upload">Upload</Link></div>
             <div className="row">
                 {this.state.documents.map(document =>
                     <div key={document.id} className="col-lg-3 col-md-3 col-xs-3">
-                        <a className="thumbnail" href={"/read/" + document.type + "/" + document.id }>
-                            <img className="img-responsive" src={"/file/" + document.cover_id} alt="" />
+                        <Link className="thumbnail" to={"/read/" + document.type + "/" + document.id }>
+                            <img className="img-responsive" src={"/api/files/" + document.cover_id} alt="" />
                             <p>{ document.name }</p>
-                        </a>
+                        </Link>
                         <p>
                             <a href={"/metadata/" + document.type + "/" + document.id}><span className="glyphicon glyphicon-edit"></span> Information</a>
                             <a href={"/download/" + document.type + "/" + document.id}><span className="glyphicon glyphicon-download-alt"></span> Download</a>
